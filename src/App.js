d718f2f3e028a3638243ff46c4e7cbb26ceef4e7
alicankocman <kocmanalican@gmail.com>
2024-07-11 16:42:03 +0300

    components added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import IlanList from './components/IlanList';
+import JobDetail from './components/JobDetail';
 import './App.css';
-import IlanList from "./components/IlanList";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="App">
-     <h1>İş İlan Portalı</h1>
-     <div className="header">
-      <div className="arama_kutusu">
-        <input type="text" placeholder="İş İlanı Arayın" />
-        <button id="arama_butonu">Ara</button>
+    <Router>
+      <div className="App">
+        <h1>İş İlan Portalı</h1>
+        <div className="header">
+          <div className="arama_kutusu">
+            <input
+              type="text"
+              placeholder="İş İlanı Arayın"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button id="arama_butonu">Ara</button>
+          </div>
+        </div>
+        <Routes>
+          <Route path="/" element={<IlanList searchTerm={searchTerm} />} />
+          <Route path="/job/:jobId" element={<JobDetail />} />
+        </Routes>
       </div>
-     </div>
-      <IlanList />
-    </div>
+    </Router>
   );
 }
 
